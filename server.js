abfb8fa42fe95634787208f1cc12c0776e9aa1bb
Tarasova Anna <tarasavahanna@gmail.com>
2024-09-18 18:57:09 +0300

    правки

--- server.js
+++ server.js
@@ -3,15 +3,9 @@ const cors = require("cors");
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/users");
 
-const corsOptions = {
-  origin: "https://frontend-pink-six.vercel.app/", // замените на URL вашего фронтенда
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-};
-
 const app = express();
+app.use(cors());
 app.use(express.json());
-app.use(cors(corsOptions));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
