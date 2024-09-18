91d6df0dd68525ad8ae18a2ec38b4d0e00860026
Tarasova Anna <tarasavahanna@gmail.com>
2024-09-19 00:47:43 +0300

    fix

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/users");
 
 const app = express();
-app.use(cors({ origin: "https://frontend-pink-six.vercel.app" }));
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
