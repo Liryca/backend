2e525ed567a8a02f429abf1c89015d0404e4e649
Tarasova Anna <tarasavahanna@gmail.com>
2024-09-19 08:52:03 +0300

    fix

--- api/index.js
+++ api/index.js
@@ -10,11 +10,9 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 
-module.exports = (req, res) => {
-  app(req, res);
-};
-
 // const PORT = process.env.PORT || 5000;
 // app.listen(PORT, () => {
 //   console.log(`Server is running on port ${PORT}`);
 // });
+
+module.exports = app;
