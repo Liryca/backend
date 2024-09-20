06a8d609094b061653e8d07c258beff7d421c55d
Tarasova Anna <tarasavahanna@gmail.com>
2024-09-20 16:36:01 +0300

    fix

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const port = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth", authRoutes);
-app.use("/api/users", userRoutes);
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
 
 app.listen(port, "0.0.0.0", () => {
   console.log(`Example app listening on port ${port}`);
