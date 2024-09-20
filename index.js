db2696db75ffca87177a53432756ffbd723a80e3
Tarasova Anna <tarasavahanna@gmail.com>
2024-09-20 12:41:02 +0300

    fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const userRoutes = require("./api/users");
 require("dotenv").config();
 const app = express();
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -13,6 +13,6 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 
-app.listen(() => {
+app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
